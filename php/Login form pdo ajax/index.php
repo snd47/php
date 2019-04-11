60c0fa921bf0ad481edb70e097fcad8b08d036df
Oleksandr Kononenko <s_com@ukr.net>
2019-04-11 21:25:14 +0300

    Login form, pdo, ajax

---
+++ php/Login form pdo ajax/index.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    
+    <title>Login</title>
+    <link type="text/css" rel="stylesheet" href="./css/style.css">
+    <script type="text/javascript" src="./js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="./js/javascript.js"></script>
+</head>
+<body>
+
+    <div id="container">
+        <div class="response"></div>
+        <form id="login" action="" method="POST">
+            <input type="text" name="nickname" placeholder="YourNickname">
+            <input type="password" name ="password" placeholder="YourPassword">
+            <input type="submit" value="Enter">
+        </form>
+    </div>
+</body>
+</html>
